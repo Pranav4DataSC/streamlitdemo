8d7ba299fef7f1b8a75cbc5f98017c823a245f73
Pranav4DataSC <119378593+Pranav4DataSC@users.noreply.github.com>
2023-04-24 08:30:26 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,47 @@
+import os
+
+import altair as alt
+import numpy as np
+import pandas as pd
+import pydeck as pdk
+import streamlit as st
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="NYC Ridesharing Demo", page_icon=":taxi:")
+
+
+# LOAD DATA ONCE
+#@st.cache(persist=True)
+def load_data(nrows):
+    path = "uber-raw-data-sep14.csv.gz"
+    if not os.path.isfile(path):
+        path = f"https://github.com/streamlit/demo-uber-nyc-pickups/raw/main/{path}"
+
+    data = pd.read_csv(
+        path,
+        nrows=100000,  # approx. 10% of data
+        names=[
+            "date/time",
+            "lat",
+            "lon",
+        ],  # specify names directly since they don't change
+        skiprows=1,  # don't read header since names specified directly
+        usecols=[0, 1, 2],  # doesn't load last column, constant value "B02512"
+        parse_dates=[
+            "date/time"
+        ],  # set as datetime instead of converting after the fact
+    )
+
+    return data
+# STREAMLIT APP LAYOUT
+data = load_data(1000)
+
+##hour = 12
+##data = data[data[DATE_TIME].dt.hour == hour]
+
+st.map(data)
+
+'data', data
+
+if st.checkbox('Show Raw Data'):
+	'## Raw Data at %sh'% hour, data
